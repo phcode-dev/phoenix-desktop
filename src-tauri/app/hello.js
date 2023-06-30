67c8f18769e5c7ffe414ff13c2012dc4a40bd368
Charly Abraham <charly@core.ai>
2023-06-30 22:17:14 +0530

    feat: initial code to execute node from sidecar

---
+++ src-tauri/app/hello.js
@@ -0,0 +1,7 @@
+import * as http from "http";
+
+//create a server object:
+http.createServer(function (req, res) {
+    res.write('Hello World!'); //write a response to the client
+    res.end(); //end the response
+}).listen(8080); //
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,23 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+all(not(debug_assertions), target_os = "windows"),
+windows_subsystem = "windows"
 )]
+
+use tauri::api::process::Command;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
+fn greet(handle: tauri::AppHandle) -> String {
+    let resource_path = handle.path_resolver()
+        .resolve_resource("app/hello.js")
+        .expect("failed to resolve resource");
+    Command::new_sidecar("node")
+        .expect("failed to create `my-sidecar` binary command")
+        .args(resource_path.as_path().to_str())
+        .spawn()
+        .expect("Failed to spawn sidecar");
+    let name = "hello";
+    println!("hello");
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
