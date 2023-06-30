71e5ba5915ee447de472beff1c703290d21bdaad
Charly Abraham <charly@core.ai>
2023-07-01 00:27:22 +0530

    fix: node not loading

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,7 +7,7 @@ use tauri::api::process::Command;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(handle: tauri::AppHandle) -> String {
+fn greet(handle: tauri::AppHandle, name: &str) -> String {
     let resource_path = handle.path_resolver()
         .resolve_resource("app/hello.js")
         .expect("failed to resolve resource");
@@ -16,7 +16,7 @@ fn greet(handle: tauri::AppHandle) -> String {
         .args(resource_path.as_path().to_str())
         .spawn()
         .expect("Failed to spawn sidecar");
-    let name = "hello";
+   // let name = "hello";
     println!("hello");
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
