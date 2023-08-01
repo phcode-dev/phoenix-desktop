dc9089218145d52ba323a9824ea6a0c755410e17
abose <arun@core.ai>
2023-08-01 18:16:07 +0530

    feat: add toggle_devtools api in tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,19 +11,32 @@ fn greet(handle: tauri::AppHandle, name: &str) -> String {
     let resource_path = handle.path_resolver()
         .resolve_resource("app/hello.js")
         .expect("failed to resolve resource");
-    Command::new_sidecar("node")
+    Command::new_sidecar("phnode")
         .expect("failed to create `my-sidecar` binary command")
         .args(resource_path.as_path().to_str())
         .spawn()
         .expect("Failed to spawn sidecar");
-   // let name = "hello";
     println!("hello");
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn toggle_devtools(window: tauri::Window) {
+    static mut DEVTOOLS_LOADED:bool = false;
+    unsafe {
+        // though unsafe, this is fine as its just a view toggle and not mission critical.
+        if !DEVTOOLS_LOADED {
+            window.open_devtools();
+        } else {
+            window.close_devtools();
+        }
+        DEVTOOLS_LOADED = !DEVTOOLS_LOADED;
+    }
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, toggle_devtools])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
