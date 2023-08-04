8b3238917c406b9f56075693d7a48fe6cab88734
abose <arun@core.ai>
2023-08-04 15:49:17 +0530

    fix: crash on startup in mac and windows

--- src-tauri/src/init.rs
+++ src-tauri/src/init.rs
@@ -39,6 +39,8 @@ fn restore_window_size(app: &mut tauri::App, boot_config: &BootConfig){
 
 pub fn init_app(app: &mut tauri::App) {
     let config = app.config().clone();
+    println!("Appdata path is {}",  tauri::api::path::app_data_dir(&config).expect("failed to retrieve app_data_dir").display());
+    ensure_dir_exists(&tauri::api::path::app_data_dir(&config).unwrap()); // canonicalize will work only if path exists
     let _ = APP_CONSTANTS.set(AppConstants {
         tauri_config: config.clone(),
         app_data_dir: tauri::api::path::app_data_dir(&config).expect("failed to retrieve app_data_dir")
@@ -48,7 +50,6 @@ pub fn init_app(app: &mut tauri::App) {
     // To get a value
     if let Some(app_constants) = APP_CONSTANTS.get() {
         println!("Bundle ID is {}", app_constants.tauri_config.tauri.bundle.identifier);
-        println!("Appdata Dir is {}", app_constants.app_data_dir.display());
         ensure_dir_exists(&app_constants.app_data_dir);
         let boot_config = read_boot_config();
         restore_window_size(app, &boot_config);
