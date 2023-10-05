a648e9a145425bc08e0a045d2b1ea26fb814efb5
abose <arun@core.ai>
2023-10-05 13:10:12 +0530

    build: create releaseDistTest target to build phoenix tester bins

---
+++ src-build/createDistTestReleaseConfig.js
@@ -0,0 +1,25 @@
+import {fileURLToPath} from "url";
+import {dirname} from "path";
+import fs from 'fs';
+
+import {getPlatformDetails} from "./utils.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+async function createDistTestReleaseConfig() {
+    const platform = getPlatformDetails().platform;
+    const tauriConfigPath = (platform === "win") ? `${__dirname}\\..\\src-tauri\\tauri.conf.json`
+        : `${__dirname}/../src-tauri/tauri.conf.json`;
+    const tauriLocalConfigPath = (platform === "win") ? `${__dirname}\\..\\src-tauri\\tauri-local.conf.json`
+        : `${__dirname}/../src-tauri/tauri-local.conf.json`;
+    console.log("Reading config file: ", tauriConfigPath);
+    let configJson = JSON.parse(fs.readFileSync(tauriConfigPath));
+    console.log('\x1b[34m%s\x1b[0m', "\n!Only creating executables. Creating msi, appimage and dmg installers are disabled in this build. If you want to create an installer, use: npm run tauri build manually after setting distDir in tauri conf!\n")
+    configJson.tauri.bundle.active = false;
+    configJson.build.distDir = '../../phoenix/dist-test/'
+    console.log("Writing new local config json ", tauriLocalConfigPath);
+    fs.writeFileSync(tauriLocalConfigPath, JSON.stringify(configJson, null, 4));
+}
+
+await createDistTestReleaseConfig();
