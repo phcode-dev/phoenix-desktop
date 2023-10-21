e03db509108555f85c205593f8295ee5720e9f4a
abose <arun@core.ai>
2023-10-21 22:11:46 +0530

    ci: add integ test run on pull requests

---
+++ src-build/clonePhoenixForTests.js
@@ -0,0 +1,34 @@
+import {$} from 'execa'; // https://www.npmjs.com/package/execa
+import fs from 'fs';
+import path from 'path'
+
+console.log("Cloning phoenix...");
+async function pwd() {
+    const pwd =  process.cwd();
+    console.log("current dir: ", pwd);
+    return pwd;
+}
+
+const projectDir = await pwd();
+const phoenixProjectPath = path.join(projectDir, "..", "phoenix");
+
+let packageJSONPath = path.join(projectDir, 'package.json');
+console.log('loading package.json: ', packageJSONPath);
+
+let packageJSON = JSON.parse(fs.readFileSync(packageJSONPath, 'utf-8'));
+const phoenixRepo = packageJSON.phoenixRepo;
+
+process.chdir('..');
+await pwd();
+
+console.log(`Cloning`, phoenixRepo);
+if(!fs.existsSync(phoenixProjectPath)){
+    await $`git clone ${phoenixRepo.gitClonrUrl} --branch ${phoenixRepo.branch}`;
+}
+process.chdir('phoenix');
+await pwd();
+console.log('checking out commit: ', phoenixRepo.commit);
+await $`git checkout ${phoenixRepo.commit}`;
+
+process.chdir(projectDir);
+await pwd();
