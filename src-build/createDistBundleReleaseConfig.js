d565f76cc968f5609b52d8d6a72a941f2a46a3b7
abose <arun@core.ai>
2024-02-11 12:20:18 +0530

    build: releaseDistBundle command to build bundles

--- src-build/createDistBundleReleaseConfig.js
+++ src-build/createDistBundleReleaseConfig.js
@@ -1,34 +1,36 @@
 import {fileURLToPath} from "url";
 import {dirname} from "path";
 import fs from 'fs';
-import * as os from 'os';
+import chalk from 'chalk';
 
-import {getPlatformDetails} from "./utils.js";
-import chalk from "chalk";
+import {getPlatformDetails, patchTauriConfigWithMetricsHTML} from "./utils.js";
+import os from "os";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-async function createSrcReleaseConfig() {
+async function createDistReleaseConfig() {
     const platform = getPlatformDetails().platform;
     const tauriConfigPath = (platform === "win") ? `${__dirname}\\..\\src-tauri\\tauri.conf.json`
         : `${__dirname}/../src-tauri/tauri.conf.json`;
     const tauriLocalConfigPath = (platform === "win") ? `${__dirname}\\..\\src-tauri\\tauri-local.conf.json`
         : `${__dirname}/../src-tauri/tauri-local.conf.json`;
-    console.log("Reading config file: ", tauriConfigPath);
+    console.log("Reading Tauri config file: ", tauriConfigPath);
     let configJson = JSON.parse(fs.readFileSync(tauriConfigPath));
-    console.log(chalk.cyan("\n!Only creating executables. Creating msi, appimage and dmg installers are disabled in this build. If you want to create an installer, use: npm run releaseDistBundle\n"));
-    configJson.tauri.bundle.active = false;
-    configJson.build.distDir = '../../phoenix/src/'
+    console.log(chalk.cyan("\n!Updates and signing is disabled while creating msi, appimage and dmg installers. If you want to sign, use tauri build commands directly.\n"));
+    configJson.tauri.bundle.active = true;
+    configJson.tauri.updater.active = false;
+    configJson.build.distDir = '../../phoenix/dist/';
     const phoenixVersion = configJson.package.version;
     if(os.platform() === 'win32'){
         configJson.tauri.windows[0].url = `https://phtauri.localhost/v${phoenixVersion}/`;
     } else {
         configJson.tauri.windows[0].url = `phtauri://localhost/v${phoenixVersion}/`;
     }
+    patchTauriConfigWithMetricsHTML(configJson);
     console.log("Window Boot url is: ", configJson.tauri.windows[0].url);
     console.log("Writing new local config json ", tauriLocalConfigPath);
     fs.writeFileSync(tauriLocalConfigPath, JSON.stringify(configJson, null, 4));
 }
 
-await createSrcReleaseConfig();
+await createDistReleaseConfig();
